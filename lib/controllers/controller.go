941f20d11576f4248a71ec9abc4ad153fe32a996
Javad Bayzavi <javadbayzavi@gmail.com>
2022-05-06 04:56:42 -0700

    reconfiguration

--- lib/controllers/controller.go
+++ lib/controllers/controller.go
@@ -9,6 +9,7 @@ package Controllers
 
 import (
 	"go-microservices/lib/Controllers/ControllerInterfaces"
+	"go-microservices/lib/Controllers/Executors"
 	"go-microservices/lib/Controllers/Executors/ExecutorInterfaces"
 	"net/http"
 )
@@ -19,8 +20,13 @@ type Controller struct {
 }
 
 //Create a generic http response helper for all incoming requests
-func (this *Controller) Run(params map[string]string) http.HandlerFunc {
-	
+func (this *Controller) Run(params map[string]string) (http.HandlerFunc, error) {
+
+	//Checking for the configuration of the execution engine
+	if this.executor == nil {
+		this.executor = Executors.Executor{target: this}
+	}
+
 	return func(rw http.ResponseWriter, r *http.Request) {
 
 		rw.Header().Add("content-type", "application/json")
